d7f6281055291453679797898e9997694047782e
Vara <artemiivoropaev@gmail.com>
2025-08-04 16:05:45 +0700

    fail build

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/9exari5i8bx2q8ag?svg=true)](https://ci.appveyor.com/project/Vorapay/postman-echo)
---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void testPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello, Postman!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("wrongField", equalTo("Hello, Postman!"));
+    }
+}
